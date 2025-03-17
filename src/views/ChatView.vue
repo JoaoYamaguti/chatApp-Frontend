e0d804086f477698c6d6589f18a5b78fd29ace00
JoaoYamaguti <joaov.yamaguti@gmail.com>
2025-03-16 21:12:34 -0300

    feat: working in progress on chat page

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -3,17 +3,32 @@ import { ref } from "vue";
 import FooterComponent from "../components/FooterComponent.vue";
 import HeaderComponent from "../components/HeaderComponent.vue";
 
+const baseURL = import.meta.env.VITE_API_URL
+const token = sessionStorage.getItem("token")
+
 const chats = ref([])
 
-async function getChats () {
+async function getChats() {
+  console.log(token)
+  const res = await (await fetch(baseURL + "/chat", {
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer " + token
+    }
+  })).json()
 
+  chats.value = res.chats
+
+  console.log(res.chats)
 }
+
+getChats()
 
 </script>
 
 <template>
   <div class="container">
-    <HeaderComponent/>
+    <HeaderComponent />
 
     <nav>
       <div>
@@ -24,12 +39,16 @@ async function getChats () {
     </nav>
 
     <aside>
-
+      <ul>
+        <li v-for="chat in chats" :key="index">
+          <h3>{{chat.name}}</h3>
+        </li>
+      </ul>
     </aside>
 
     <main>main</main>
 
-    <FooterComponent/>
+    <FooterComponent />
   </div>
 </template>
 
